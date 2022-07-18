ff163cd557145b100022002f848cd5bf7ecdf8e2
Vedant Singhania <vedant.singhania@gmail.com>
2022-07-18 10:57:20 -0700

    longer decimals

--- pi.py
+++ pi.py
@@ -1,17 +1,19 @@
-def pi(length, accuracy):
+from decimal import getcontext, Decimal
+
+def pi(length, reps):
 
   """
   Calculates the value of pi using Nilakantha's series.
   """
-
-  pi = 0
-  top_num = 4 * length
+  getcontext().prec = length
+  pi = 3
+  top_num = 4
   bot_num = 2
   positive = True
 
-  for _ in range(1, accuracy):
+  for _ in range(1, reps):
     new_bot = bot_num*(bot_num+1)*(bot_num+2)
-    num = top_num//new_bot
+    num = Decimal(top_num)/Decimal(new_bot)
     if positive:
       pi += num
     else:
@@ -19,12 +21,13 @@ def pi(length, accuracy):
     positive = not positive
     bot_num += 2
 
-  print("3.", round(pi), sep="")
+  return pi
 
-length = 10**100
-accuracy = 10**7
+length = 10**2
+reps = 10**8
 
-pi(length, accuracy)
+pi_calculated = pi(length, reps)
+print(pi_calculated)
+
 pi_hundred = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
 print(pi_hundred)
-print("21 digit accuracy with 10**100 length and 10**7 accuracy")
